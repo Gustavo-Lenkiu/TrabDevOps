ecf476e44900729645d9d04910471ab5909bbf32
Gustavo-Lenkiu <gustavocoelho.lenkiu@gmail.com>
2025-08-15 19:51:20 -0300

    Outro teste

--- main.py
+++ main.py
@@ -11,4 +11,4 @@ async def root():
 # http://127.0.0.1:8000/teste1
 @app.get("/teste")
 async def funcaoteste():
-    return{"Teste": True, "num_aleatório" : random.randint(0, 1000)}
+    return{"Teste": True, "num_aleatório" : random.randint(0, 20000)}
