bcd4140321b9df4b66efbdbf147a20a017191ce8
Gustavo-Lenkiu <gustavocoelho.lenkiu@gmail.com>
2025-09-22 18:11:35 -0300

    teste 5

--- teste/teste.py
+++ teste/teste.py
@@ -5,25 +5,38 @@ from unittest.mock import patch
 
 @pytest.mark.asyncio
 def teste_root():
-    assert root()== {"message": "Hello World"}
+    result = root()
+    yield result
+    assert result == {"message": "Hello World"}
 
 def teste_funcaoteste():
     with patch('random.randint', return_value=25000):
         result = funcaoteste()
+        yield result
     assert result == {"Teste": True, "num_aleatório" : 25000}
 
 def teste_cadastrar_estudante(estudante: Estudante):
     estudante_teste = Estudante(nome="Gustavo", curso="ADS", ativo=True)
-    assert estudante_teste == cadastrar_estudante(estudante_teste)
+    result = cadastrar_estudante(estudante_teste)
+    yield result
+    assert estudante_teste == result
 
 def teste_atualizar_estudante_negativo():
-    assert not atualizar_estudante(-1)
+    result = atualizar_estudante(-1)
+    yield result
+    assert not result
 
 def teste_atualizar_estudante_positivo():
-    assert atualizar_estudante(1)
+    result = atualizar_estudante(1)
+    yield result
+    assert result
 
 def teste_deletar_estudante_negativo():
-    assert not deletar_estudante(-1)
+    result = deletar_estudante(-1)
+    yield result
+    assert not result
 
 def teste_deletar_estudante_positivo():
-    assert deletar_estudante(1)
+    result = deletar_estudante(1)
+    yield result
+    assert result
